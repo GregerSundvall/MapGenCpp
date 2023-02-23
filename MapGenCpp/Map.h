30166665cbb147ec8b1c28ab7680761b9b857b52
GregerSundvall <71319618+GregerSundvall@users.noreply.github.com>
2023-02-23 14:30:48 +0100

    Just started

---
+++ MapGenCpp/Map.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+#include <iostream>
+#include <vector>
+
+#include "Door.h"
+#include "Room.h"
+
+class Map
+{
+    int width;
+    int height;
+
+    std::vector<Room> rooms;
+    std::vector<Door> doors;
+    std::vector<std::vector<char>> map;
+
+    
+public:
+    Map(int width, int height)
+    {
+        this->width = width;
+        this->height = height;
+
+        FillMapWith('X');
+    }
+
+    void FillMapWith(char character)
+    {
+        for (int w = 0; w < width; ++w)
+        {
+            std::vector<char> vector;
+            for (int h = 0; h < height; ++h)
+            {
+                vector.push_back(character);
+            }
+            map.push_back(vector);
+        }
+    }
+    
+    void PrintMapToConsole()
+    {
+        for (int h = 0; h < height; ++h)
+        {
+            for (int w = 0; w < width; ++w)
+            {
+                std::cout << map[w][h];
+            }
+            std::cout << std::endl;
+        }
+    }
+    
+};
---
+++ MapGenCpp/MapGenCpp.cpp
@@ -0,0 +1,10 @@
+
+#include "Map.h"
+
+int main(int argc, char* argv[])
+{
+    Map* map = new Map(79, 14);
+    map->PrintMapToConsole();
+    
+    return 0;
+}
